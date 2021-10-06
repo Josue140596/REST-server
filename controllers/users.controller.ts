2a9a7f558f60ab41c39b9c885b5164a736d36e0b
Josue140596 <josue140596@gmial.com>
2021-10-05 22:33:08 -0500

    [SP2] Working with delete user and get users

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -6,26 +6,26 @@ import bcryptjs from 'bcryptjs';
 import Usuario from '../models/Usuario';
 
 
-export const getUser = async(req: Request, res: Response) => {
-  const {limit, since } = req.query;
-  const query = {estado: true}
+export const getUser = async (req: Request, res: Response) => {
+  const { limit, since } = req.query;
+  const query = { estado: true }
 
   const [total, users] = await Promise.all([
     User.countDocuments(query),
     User.find(query)
-    .skip(Number(since) || 0)
-    .limit(Number(limit) || 5)
+      .skip(Number(since) || 0)
+      .limit(Number(limit) || 5)
   ]);
   res.json({
     total,
     users
   });
 }
-export const postUser = async(req: Request, res: Response)  => {
-  const {name, email, password, rol} = req.body
-  const user = new User({name, email, password, rol});
+export const postUser = async (req: Request, res: Response) => {
+  const { name, email, password, rol } = req.body
+  const user = new User({ name, email, password, rol });
   //Encript Password
-    //How many salts do you want?
+  //How many salts do you want?
   const salt = bcryptjs.genSaltSync(10);
   user.password = bcryptjs.hashSync(password, salt)
   //Save in DB
@@ -35,7 +35,7 @@ export const postUser = async(req: Request, res: Response)  => {
   });
 
 }
-export const putUser = async(req: Request, res: Response) => {
+export const putUser = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { _id, password, byGoogle, email, ...resto } = req.body;
   //TODO: Validate with DB
@@ -46,8 +46,8 @@ export const putUser = async(req: Request, res: Response) => {
   const user = await Usuario.findByIdAndUpdate(id, resto)
   res.json(user);
 }
-export const deleteUser = async(req: Request, res: Response) => {
-  const {id} = req.params;
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
   //Delete 
   const user = await User.findByIdAndDelete(id)
   res.json(user);
